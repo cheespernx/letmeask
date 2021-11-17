ec215c837a65dca704bfe3d37b4327ce413da7ab
Victor Carvalho <contato@cheesper.com>
2021-11-17 13:32:49 -0400

    Day 2 (Added Auth with Firebase)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { Button } from './components/Button'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <Button></Button>
+    <Router>
+      <AuthContextProvider>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+        </AuthContextProvider>
+    </Router>
   );
 }
 
